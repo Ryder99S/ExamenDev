5cd9e0cf8339a153b088bf9b87451d40f5e52e1d
Ridito <riderjavierposadassandoval@gmail.com>
2021-11-17 12:36:53 -0600

    Arreglo Templates

--- ProyectoExamen/settings.py
+++ ProyectoExamen/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'ProyectoExamen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'ExamenApp/Templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
