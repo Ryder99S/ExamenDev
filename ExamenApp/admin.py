84750d3a32827652682325fe1e013cbf663ed09e
Ridito <riderjavierposadassandoval@gmail.com>
2021-11-17 22:20:22 -0600

    Creacion de Modelos Sitios

--- ExamenApp/admin.py
+++ ExamenApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.apps import apps
 # Register your models here.
+
+myapp = apps.get_app_config("ExamenApp")
+
+for model_name, model in myapp.models.items():
+	admin.site.register(model)
--- ExamenApp/models.py
+++ ExamenApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Sitios(models.Model):
+    descripcion = models.CharField(max_length=80,null = False,blank=False)
+    longitud = models.FloatField(null=False, blank=False)
+    latitud = models.FloatField(null=False, blank=False)
+    fotografia =  models.BinaryField(blank = False, null = False, editable = True)
+
+    def __str__(self):
+        return "{}-{}-{}-{}".format(self.pk,self.descripcion,self.longitud,self.latitud,self.fotografia)
